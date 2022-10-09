f302eb8d86b742e93995e34dd28b7052ee41da79
Luis José Llamas Pérez <luisjllamasperez@gmail.com>
2022-10-09 21:26:06 +0200

    Finished all functional requirements

--- src/stringcalculator.ts
+++ src/stringcalculator.ts
@@ -1,111 +1,20 @@
 export default class StringCalculator {
+  //-- Separator --
   defaultSeparator = ','
   validSeparators = ['\n']
-  customSeparatorBegin = '//'
-  customSeparatorEnd = '\n'
+  customSeparatorDeclarationBegin = '//'
+  customSeparatorDeclarationEnd = '\n'
+  customSeparatorBegin = '['
+  customSeparatorEnd = ']'
+  //-- Special Characters --
+  specialCharacters = ['*', '\\', '[', '(', '.', '?', '+']
+  //-- Errors --
   errorNegativeNumbersDescription = 'negativos no soportados: '
-  special_characters = ['*', '\\', '[', '(', '.', '?', '+']
+  errorMalformedCustomSeparatorDescription = 'El separador personalizado está mal formado.'
 
-  isEmpty(numbers: number[]) {
-    return numbers.length < 1 || isNaN(numbers[0])
-  }
+  //-- Functions/Methods --
 
-  hasMoreThan1Number(numbers: number[]) {
-    return numbers.length > 1
-  }
-
-  obtainNumbersFromString(numbers: string) {
-    const numbersWithDefaultSeparator = this.replaceSeparatorsWithDefaultSeparator(numbers)
-    return numbersWithDefaultSeparator.split(this.defaultSeparator).map(numero => parseInt(numero))
-  }
-
-  obtainNegativeNumbers(numbers: number[]) {
-    return numbers.filter(value => value < 0)
-  }
-
-  hasNegativeNumbers(numbers: number[]) {
-    return numbers.some(number => number < 0)
-  }
-
-  throwNegativeNumbersError(numbers: number[]) {
-    const negativeNumbers = this.obtainNegativeNumbers(numbers)
-    throw new Error(this.errorNegativeNumbersDescription + negativeNumbers)
-  }
-
-  obtainSumOfArrayOfNumbers(numbers: number[]) {
-    return numbers.reduce((partialSum, number) => partialSum + number, 0)
-  }
-
-  haveCustomDelimiter(numbers: string) {
-    return numbers.startsWith(this.customSeparatorBegin)
-  }
-
-  obtainIndexEndOfCustomSeparator(numbers: string) {
-    return numbers.indexOf(this.customSeparatorEnd)
-  }
-
-  isCustomSeparatorMultiCharacter(
-    numbers: string,
-    beginCustomSeparator: number,
-    endOfCustomSeparatorDeclaration: number
-  ) {
-    return numbers[beginCustomSeparator] === '[' && numbers[endOfCustomSeparatorDeclaration - 1] === ']'
-  }
-
-  addCustomSeparatorToSeparators(numbers: string) {
-    let beginCustomSeparator = this.customSeparatorBegin.length
-    let endCustomSeparator = this.obtainIndexEndOfCustomSeparator(numbers)
-    if (this.isCustomSeparatorMultiCharacter(numbers, beginCustomSeparator, endCustomSeparator)) {
-      beginCustomSeparator += 1
-      endCustomSeparator -= 1
-    }
-    this.validSeparators.push(numbers.substring(beginCustomSeparator, endCustomSeparator))
-  }
-
-  removeCustomSeparatorDeclarationFromString(numbers: string) {
-    return numbers.substring(this.obtainIndexEndOfCustomSeparator(numbers) + 1)
-  }
-
-  customSeparatorHasSpecialCharacters(separator: string) {
-    return this.special_characters.some(character => separator.includes(character))
-  }
-
-  resetValidSeparatos() {
-    this.validSeparators = ['\n']
-  }
-
-  obtainSpecialCharactersInCustomSeparator(separator: string){
-    return this.special_characters.filter(character => separator.includes(character))
-  }
-
-  replaceSeparatorsWithDefaultSeparator(numbers: string) {
-    this.validSeparators.forEach(separator => {
-      if (numbers.includes(separator)) {
-        if (this.customSeparatorHasSpecialCharacters(separator)) {
-          const special_characters = this.obtainSpecialCharactersInCustomSeparator(separator)
-          special_characters.forEach(character => {
-            separator = separator.replace(new RegExp(`\\${character}`,'g'),'\\$&')
-            console.log(separator);
-          })
-        }
-
-        numbers = numbers.replace(new RegExp(`${separator}`, 'g'), this.defaultSeparator)
-      }
-    })
-
-    this.resetValidSeparatos()
-
-    return numbers
-  }
-
-  anyIsBiggerThan1000(numbers: number[]) {
-    return numbers.some(number => number > 1000)
-  }
-
-  removeNumbersBiggerThan1000(numbers: number[]) {
-    return numbers.filter(number => number < 1000)
-  }
-
+  //- Main Functions/Methods -
   Add(numbersInString_input: string) {
     if (this.haveCustomDelimiter(numbersInString_input)) {
       this.addCustomSeparatorToSeparators(numbersInString_input)
@@ -119,5 +28,196 @@ export default class StringCalculator {
     if (this.isEmpty(numbers)) return 0
     if (!this.hasMoreThan1Number(numbers)) return numbers[0]
     else return this.obtainSumOfArrayOfNumbers(numbers)
+  }
+
+  //-- Auxiliary Functions/Methods --
+
+  //-- Add... --
+  addCustomSeparatorWithMoreThanOneCharacter(numbers: string, beginSeparator: number, endSeparator: number) {
+    if (this.isMalformedCustomSeparator(numbers, beginSeparator, endSeparator - 1))
+      throw new Error(this.errorMalformedCustomSeparatorDescription)
+    this.validSeparators.push(numbers.substring(beginSeparator + 1, endSeparator - 1))
+  }
+
+  addCustomSeparatorToSeparators(numbers: string) {
+    const beginDeclarationOfSeparator = this.customSeparatorDeclarationBegin.length
+    const endDeclarationOfSeparator = this.obtainIndexOfCustomSeparatorDeclarationEnd(numbers)
+
+    if (!this.hasCustomSeparatorMoreThanOneCharacter(beginDeclarationOfSeparator + 1, endDeclarationOfSeparator - 1)) {
+      this.addCustomSeparatorWithOnlyOneCharacter(numbers, beginDeclarationOfSeparator)
+      return
+    }
+
+    if (!this.hasMoreThanOneCustomSeparator(numbers, beginDeclarationOfSeparator, endDeclarationOfSeparator)) {
+      this.addCustomSeparatorWithMoreThanOneCharacter(numbers, beginDeclarationOfSeparator, endDeclarationOfSeparator)
+      return
+    }
+
+    const numberOfCustomSeparators = this.obtainIndexesOfCustomSeparators(numbers)
+    numberOfCustomSeparators.forEach(([separatorBeginIndex, separatorEndIndex]) => {
+      this.addCustomSeparatorWithIndexes(numbers, separatorBeginIndex, separatorEndIndex)
+    })
+  }
+
+  addCustomSeparatorWithOnlyOneCharacter(numbers: string, beginSeparator: number) {
+    this.validSeparators.push(numbers.substring(beginSeparator, beginSeparator + 1))
+  }
+
+  addCustomSeparatorWithIndexes(numbers: string, beginSeparator: number, endSeparator: number) {
+    if (!this.hasCustomSeparatorMoreThanOneCharacter(beginSeparator + 1, endSeparator - 1)) {
+      this.addCustomSeparatorWithOnlyOneCharacter(numbers, beginSeparator + 1)
+      return
+    }
+
+    this.addCustomSeparatorWithMoreThanOneCharacter(numbers, beginSeparator, endSeparator + 1)
+  }
+
+  addEscapeCharacterToSpecialCharacter(separator: string, special_character: string) {
+    return separator.replace(new RegExp(`\\${special_character}`, 'g'), '\\$&')
+  }
+
+  //-- Any... --
+  anyIsBiggerThan1000(numbers: number[]) {
+    return numbers.some(number => number > 1000)
+  }
+
+  //-- Has... --
+  hasMoreThan1Number(numbers: number[]) {
+    return numbers.length > 1
+  }
+
+  hasNegativeNumbers(numbers: number[]) {
+    return numbers.some(number => number < 0)
+  }
+
+  hasCustomSeparatorMoreThanOneCharacter(beginCustomSeparator: number, endCustomSeparator: number) {
+    return endCustomSeparator - beginCustomSeparator >= 1
+  }
+
+  HasSpecialCharacters(separator: string) {
+    return this.specialCharacters.some(character => separator.includes(character))
+  }
+
+  hasMoreThanOneCustomSeparator(text: string, beginDeclarationOfSeparator: number, endDeclarationOfSeparator: number) {
+    let scope = 1
+    for (let i = beginDeclarationOfSeparator + 1; i < endDeclarationOfSeparator; i++) {
+      let character = text[i]
+      if (scope === 0 && character === this.customSeparatorBegin) return true
+      if (character === this.customSeparatorBegin) scope += 1
+      else if (character === this.customSeparatorEnd) scope -= 1
+    }
+    return false
+  }
+
+  //-- Have... --
+  haveCustomDelimiter(numbers: string) {
+    return numbers.startsWith(this.customSeparatorDeclarationBegin)
+  }
+
+  //-- Is... --
+  isEmpty(numbers: number[]) {
+    return numbers.length < 1 || isNaN(numbers[0])
+  }
+
+  isCustomSeparatorMultiCharacter(
+    numbers: string,
+    beginCustomSeparator: number,
+    endOfCustomSeparatorDeclaration: number
+  ) {
+    return (
+      numbers[beginCustomSeparator] === this.customSeparatorBegin &&
+      numbers[endOfCustomSeparatorDeclaration - 1] === this.customSeparatorEnd
+    )
+  }
+
+  isMalformedCustomSeparator(numbers: string, beginCustomSeparator: number, endCustomSeparator: number) {
+    return !(
+      numbers[beginCustomSeparator] === this.customSeparatorBegin &&
+      numbers[endCustomSeparator] === this.customSeparatorEnd
+    )
+  }
+
+  //-- Obtain... --
+  obtainNumbersFromString(numbers: string) {
+    const numbersWithDefaultSeparator = this.replaceSeparatorsWithDefaultSeparator(numbers)
+    return numbersWithDefaultSeparator.split(this.defaultSeparator).map(numero => parseInt(numero))
+  }
+
+  obtainNegativeNumbers(numbers: number[]) {
+    return numbers.filter(value => value < 0)
+  }
+
+  obtainSumOfArrayOfNumbers(numbers: number[]) {
+    return numbers.reduce((partialSum, number) => partialSum + number, 0)
+  }
+
+  obtainIndexOfCustomSeparatorDeclarationEnd(numbers: string) {
+    return numbers.indexOf(this.customSeparatorDeclarationEnd)
+  }
+
+  obtainIndexesOfCustomSeparators(numbers: string) {
+    const beginDeclarationOfSeparator: number = this.customSeparatorDeclarationBegin.length
+    const endDeclarationOfSeparator: number = this.obtainIndexOfCustomSeparatorDeclarationEnd(numbers)
+
+    let scope: number = 1
+    let beginIndexOfCustomseparator: number = beginDeclarationOfSeparator
+    let endIndexOfCustomSeparator: number = -1
+    const indexesOfCustomSeparators: [number, number][] = []
+    for (let i: number = beginDeclarationOfSeparator + 1; i < endDeclarationOfSeparator; i++) {
+      let character: string = numbers[i]
+      if (scope === 0 && character === this.customSeparatorBegin) beginIndexOfCustomseparator = i
+      if (character === this.customSeparatorBegin) scope += 1
+      else if (character === this.customSeparatorEnd) scope -= 1
+      if (scope === 0) {
+        endIndexOfCustomSeparator = i
+        indexesOfCustomSeparators.push([beginIndexOfCustomseparator, endIndexOfCustomSeparator])
+      }
+    }
+    if (endIndexOfCustomSeparator + 1 !== endDeclarationOfSeparator)
+      throw new Error(this.errorMalformedCustomSeparatorDescription)
+
+    return indexesOfCustomSeparators
+  }
+
+  obtainSpecialCharacters(separator: string) {
+    return this.specialCharacters.filter(character => separator.includes(character))
+  }
+
+  //-- Remove... --
+  removeCustomSeparatorDeclarationFromString(numbers: string) {
+    return numbers.substring(this.obtainIndexOfCustomSeparatorDeclarationEnd(numbers) + 1)
+  }
+
+  removeNumbersBiggerThan1000(numbers: number[]) {
+    return numbers.filter(number => number < 1000)
+  }
+
+  //-- Replace... --
+  replaceSeparatorsWithDefaultSeparator(numbers: string) {
+    this.validSeparators.forEach(separator => {
+      if (this.HasSpecialCharacters(separator)) {
+        const special_characters = this.obtainSpecialCharacters(separator)
+        special_characters.forEach(special_character => {
+          separator = this.addEscapeCharacterToSpecialCharacter(separator, special_character)
+        })
+      }
+
+      numbers = numbers.replace(new RegExp(`${separator}`, 'g'), this.defaultSeparator)
+    })
+
+    this.resetValidSeparatos()
+
+    return numbers
+  }
+
+  //-- Reset... --
+  resetValidSeparatos() {
+    this.validSeparators = ['\n']
+  }
+
+  //-- Throw... --
+  throwNegativeNumbersError(numbers: number[]) {
+    const negativeNumbers = this.obtainNegativeNumbers(numbers)
+    throw new Error(this.errorNegativeNumbersDescription + negativeNumbers)
   }
 }
--- test/stringcalculator.test.ts
+++ test/stringcalculator.test.ts
@@ -1,130 +1,196 @@
 import StringCalculator from '../src/stringcalculator'
 
-describe('FizzBuzz', () => {
+describe('StringCalculator', () => {
   const stringCalculator = new StringCalculator()
+  describe('1.- Functionality Tests', () => {
+    const cases_base_no_number: [string, number][] = [['', 0]]
+    test.each(cases_base_no_number)('given %p returns %p', (firstArg, expectedResult) => {
+      const result = stringCalculator.Add(firstArg)
+      expect(result).toEqual(expectedResult)
+    })
 
-  const cases_base_no_number: [string, number][] = [['', 0]]
-  test.each(cases_base_no_number)('given %p returns %p', (firstArg, expectedResult) => {
-    const result = stringCalculator.Add(firstArg)
-    expect(result).toEqual(expectedResult)
+    const cases_base_1_number: [string, number][] = [
+      ['1', 1],
+      ['3', 3]
+    ]
+    test.each(cases_base_1_number)('given %p returns %p', (firstArg, expectedResult) => {
+      const result = stringCalculator.Add(firstArg)
+      expect(result).toEqual(expectedResult)
+    })
+
+    const cases_base_2_numbers: [string, number][] = [
+      ['1,2', 3],
+      ['3,4', 7]
+    ]
+    test.each(cases_base_2_numbers)('given %p returns %p', (firstArg, expectedResult) => {
+      const result = stringCalculator.Add(firstArg)
+      expect(result).toEqual(expectedResult)
+    })
+  })
+  describe('2.- Functionality Tests', () => {
+    const cases_base_more_than_2_numbers: [string, number][] = [
+      ['1,2,3,4', 10],
+      ['1,2,3,4,5,6,7,8,9,10', 55]
+    ]
+    test.each(cases_base_more_than_2_numbers)('given %p returns %p', (firstArg, expectedResult) => {
+      const result = stringCalculator.Add(firstArg)
+      expect(result).toEqual(expectedResult)
+    })
   })
 
-  const cases_base_1_number: [string, number][] = [
-    ['1', 1],
-    ['3', 3]
-  ]
-  test.each(cases_base_1_number)('given %p returns %p', (firstArg, expectedResult) => {
-    const result = stringCalculator.Add(firstArg)
-    expect(result).toEqual(expectedResult)
+  describe('3.- Functionality Tests', () => {
+    const cases_base_2_numbers_different_separator: [string, number][] = [
+      ['1\n2', 3],
+      ['4\n5', 9]
+    ]
+    test.each(cases_base_2_numbers_different_separator)('given %p returns %p', (firstArg, expectedResult) => {
+      const result = stringCalculator.Add(firstArg)
+      expect(result).toEqual(expectedResult)
+    })
+
+    const cases_base_3_numbers_different_separator: [string, number][] = [
+      ['1\n2,3', 6],
+      ['3,4\n5', 12]
+    ]
+    test.each(cases_base_3_numbers_different_separator)('given %p returns %p', (firstArg, expectedResult) => {
+      const result = stringCalculator.Add(firstArg)
+      expect(result).toEqual(expectedResult)
+    })
+
+    const cases_base_1_number_not_valid_separator: [string, number][] = [
+      ['1\n,', NaN],
+      ['3,\n', NaN]
+    ]
+    test.each(cases_base_1_number_not_valid_separator)('given %p returns %p', (firstArg, expectedResult) => {
+      const result = stringCalculator.Add(firstArg)
+      expect(result).toEqual(expectedResult)
+    })
+    ;('//;\n1;2')
   })
 
-  const cases_base_2_numbers: [string, number][] = [
-    ['1,2', 3],
-    ['3,4', 7]
-  ]
-  test.each(cases_base_2_numbers)('given %p returns %p', (firstArg, expectedResult) => {
-    const result = stringCalculator.Add(firstArg)
-    expect(result).toEqual(expectedResult)
+  describe('4.- Functionality Tests', () => {
+    const cases_base_3_numbers_custom_separator: [string, number][] = [
+      ['//;\n1;2', 3],
+      ['// \n1 2', 3],
+      ['//[\n1[2', 3],
+      ['//!\n1!2', 3],
+      ['//_\n1_2_3', 6],
+      ['//@\n1@2@4', 7],
+      ['//*\n1*2', 3],
+      ['//(\n1(2', 3],
+      ['//.\n1.2', 3],
+      ['//?\n1?2', 3],
+      ['//+\n1+2', 3],
+      ['///\n1/2', 3],
+      ['//-\n1-2', 3],
+      ['//{\n1{2', 3],
+      ['//]\n1]2', 3]
+    ]
+    test.each(cases_base_3_numbers_custom_separator)('given %p returns %p', (firstArg, expectedResult) => {
+      const result = stringCalculator.Add(firstArg)
+      expect(result).toEqual(expectedResult)
+    })
   })
 
-  const cases_base_more_than_2_numbers: [string, number][] = [
-    ['1,2,3,4', 10],
-    ['1,2,3,4,5,6,7,8,9,10', 55]
-  ]
-  test.each(cases_base_more_than_2_numbers)('given %p returns %p', (firstArg, expectedResult) => {
-    const result = stringCalculator.Add(firstArg)
-    expect(result).toEqual(expectedResult)
+  describe('5.- Functionality Tests', () => {
+    const excepcionDescription = 'negativos no soportados: '
+    const cases_base_2_negative_numbers: [string, string][] = [
+      ['-1,2', excepcionDescription + '-1'],
+      ['3,-4', excepcionDescription + '-4'],
+      ['-3,-4', excepcionDescription + '-3,-4']
+    ]
+    test.each(cases_base_2_negative_numbers)('given %p returns %p', (firstArg, expectedResult) => {
+      expect(() => {
+        return stringCalculator.Add(firstArg)
+      }).toThrow(expectedResult)
+    })
   })
 
-  const cases_base_2_numbers_different_separator: [string, number][] = [
-    ['1\n2', 3],
-    ['4\n5', 9]
-  ]
-  test.each(cases_base_2_numbers_different_separator)('given %p returns %p', (firstArg, expectedResult) => {
-    const result = stringCalculator.Add(firstArg)
-    expect(result).toEqual(expectedResult)
+  describe('6.- Functionality Tests', () => {
+    const cases_numbers_bigger_than_1000_should_be_ignored: [string, number][] = [
+      ['1,1001', 1],
+      ['1001,1002', 0],
+      ['1,2\n3,1001', 6]
+    ]
+    test.each(cases_numbers_bigger_than_1000_should_be_ignored)('given %p returns %p', (firstArg, expectedResult) => {
+      const result = stringCalculator.Add(firstArg)
+      expect(result).toEqual(expectedResult)
+    })
   })
 
-  const cases_base_3_numbers_different_separator: [string, number][] = [
-    ['1\n2,3', 6],
-    ['3,4\n5', 12]
-  ]
-  test.each(cases_base_3_numbers_different_separator)('given %p returns %p', (firstArg, expectedResult) => {
-    const result = stringCalculator.Add(firstArg)
-    expect(result).toEqual(expectedResult)
+  describe('7.- Functionality Tests', () => {
+    const cases_custom_delimiter_has_more_than_1_character: [string, number][] = [
+      ['//[,,,]\n1,,,2,,,3', 6],
+      ['//[,;]\n1,;2,;3', 6],
+      ['//[***]\n1***2***3', 6]
+    ]
+    test.each(cases_custom_delimiter_has_more_than_1_character)('given %p returns %p', (firstArg, expectedResult) => {
+      const result = stringCalculator.Add(firstArg)
+      expect(result).toEqual(expectedResult)
+    })
+
+    const errorMalformedCustomSeparatorDescription = 'El separador personalizado está mal formado.'
+    const cases_custom_delimiter_has_more_than_1_character_malformed: [string, string][] = [
+      ['//[,,,\n1,,,2,,,3', errorMalformedCustomSeparatorDescription]
+      // ['//[ \n1,;2,;3', errorMalformedCustomSeparatorDescription],
+      // ['//[***]\n1***2***3', 6]
+    ]
+    test.each(cases_custom_delimiter_has_more_than_1_character_malformed)(
+      'given %p returns %p',
+      (firstArg, expectedResult) => {
+        expect(() => {
+          return stringCalculator.Add(firstArg)
+        }).toThrow(expectedResult)
+      }
+    )
   })
 
-  const cases_base_1_number_not_valid_separator: [string, number][] = [
-    ['1\n,', NaN],
-    ['3,\n', NaN]
-  ]
-  test.each(cases_base_1_number_not_valid_separator)('given %p returns %p', (firstArg, expectedResult) => {
-    const result = stringCalculator.Add(firstArg)
-    expect(result).toEqual(expectedResult)
-  })
-  ;('//;\n1;2')
+  describe('8.- Functionality Tests', () => {
+    const cases_more_than_1_custom_delimiter: [string, number][] = [
+      ['//[;][%]\n1;2%3', 6],
+      ['//[;][%][!]\n1;2%3!4', 10]
+    ]
+    test.each(cases_more_than_1_custom_delimiter)('given %p returns %p', (firstArg, expectedResult) => {
+      const result = stringCalculator.Add(firstArg)
+      expect(result).toEqual(expectedResult)
+    })
 
-  const cases_base_3_numbers_custom_separator: [string, number][] = [
-    ['//;\n1;2', 3],
-    ['//[\n1[2', 3],
-    ['//!\n1!2', 3],
-    ['//_\n1_2_3', 6],
-    ['//@\n1@2@4', 7],
-    ['//*\n1*2', 3],
-    ['//(\n1(2', 3],
-    ['//.\n1.2', 3],
-    ['//?\n1?2', 3],
-    ['//+\n1+2', 3],
-    ['///\n1/2', 3],
-    ['//-\n1-2', 3],
-    ['//{\n1{2', 3],
-    ['//]\n1]2', 3],
-  ]
-  test.each(cases_base_3_numbers_custom_separator)('given %p returns %p', (firstArg, expectedResult) => {
-    const result = stringCalculator.Add(firstArg)
-    expect(result).toEqual(expectedResult)
+    const errorMalformedCustomSeparatorDescription = 'El separador personalizado está mal formado.'
+    const cases_custom_delimiter_has_more_than_1_character_malformed: [string, string][] = [
+      ['//[;][%\n1;2%3', errorMalformedCustomSeparatorDescription]
+    ]
+    test.each(cases_custom_delimiter_has_more_than_1_character_malformed)(
+      'given %p returns %p',
+      (firstArg, expectedResult) => {
+        expect(() => {
+          return stringCalculator.Add(firstArg)
+        }).toThrow(expectedResult)
+      }
+    )
   })
 
-  const excepcionDescription = 'negativos no soportados: '
-  const cases_base_2_negative_numbers: [string, string][] = [
-    ['-1,2', excepcionDescription+"-1"],
-    ['3,-4', excepcionDescription+"-4"],
-    ['-3,-4', excepcionDescription+"-3,-4"]
-  ]
-  test.each(cases_base_2_negative_numbers)('given %p returns %p', (firstArg, expectedResult) => {
-    expect(() => {
-      return stringCalculator.Add(firstArg)
-    }).toThrow(expectedResult)
-  })
+  describe('9.- Functionality Tests', () => {
+    const cases_more_than_1_custom_delimiter: [string, number][] = [
+      ['//[;;][%%]\n1;;2%%3', 6],
+      ['//[;;[!]][%][!]\n1;;[!]2%3!4', 10]
+    ]
+    test.each(cases_more_than_1_custom_delimiter)('given %p returns %p', (firstArg, expectedResult) => {
+      const result = stringCalculator.Add(firstArg)
+      expect(result).toEqual(expectedResult)
+    })
 
-  const cases_numbers_bigger_than_1000_should_be_ignored: [string, number][] = [
-    ['1,1001', 1],
-    ['1001,1002', 0],
-    ['1,2\n3,1001',6]
-  ]
-  test.each(cases_numbers_bigger_than_1000_should_be_ignored)('given %p returns %p', (firstArg, expectedResult) => {
-    const result = stringCalculator.Add(firstArg)
-    expect(result).toEqual(expectedResult)
+    const errorMalformedCustomSeparatorDescription = 'El separador personalizado está mal formado.'
+    const cases_custom_delimiter_has_more_than_1_character_malformed: [string, string][] = [
+      ['//[;;][%%\n1;2%3', errorMalformedCustomSeparatorDescription],
+      ['//[;;[%%\n1;2%3', errorMalformedCustomSeparatorDescription]
+    ]
+    test.each(cases_custom_delimiter_has_more_than_1_character_malformed)(
+      'given %p returns %p',
+      (firstArg, expectedResult) => {
+        expect(() => {
+          return stringCalculator.Add(firstArg)
+        }).toThrow(expectedResult)
+      }
+    )
   })
-
-  const cases_custom_delimiter_has_more_than_1_character: [string, number][] = [
-    ['//[,,,]\n1,,,2,,,3', 6],
-    ['//[,;]\n1,;2,;3', 6],
-    ['//[***]\n1***2***3',6],
-  ]
-  test.each(cases_custom_delimiter_has_more_than_1_character)('given %p returns %p', (firstArg, expectedResult) => {
-    const result = stringCalculator.Add(firstArg)
-    expect(result).toEqual(expectedResult)
-  })
-
-  const cases_more_than_1_custom_delimiter: [string, number][] = [
-    ['//[;][%]\n1;2%3', 6],
-    // ['//[,;]\n1,;2,;3', 6],
-    // ['//[***]\n1***2***3',6],
-  ]
-  test.each(cases_more_than_1_custom_delimiter)('given %p returns %p', (firstArg, expectedResult) => {
-    const result = stringCalculator.Add(firstArg)
-    expect(result).toEqual(expectedResult)
-  })
-
 })
